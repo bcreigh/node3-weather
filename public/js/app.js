9020ad5a0373d59cdd7309f84261c76bfa33e924
Bob Creigh <bcreigh@bobcreigh.com>
2020-07-18 18:04:49 -0400

    Removed console.log from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side JS file loaded');
-
 const wxform = document.querySelector('form');
 const search = document.querySelector('input');
 const msg1 = document.querySelector('#message-1');
